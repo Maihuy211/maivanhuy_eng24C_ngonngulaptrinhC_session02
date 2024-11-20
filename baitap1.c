b7fb9a8736cc61da9d3436ff760c75143d3f6d6b
Maihuy211 <maihuylc@gmail.com>
2024-11-20 20:47:50 +0700

    upload baitap1

---
+++ baitap1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+	int a=21 // kieu so nguyen
+	float a = 9.5 // kieu so thuc
+	double a= 3.3333333 // kieu so thuc co tinh chinh xac cao 
+	char kyTu = 'a'  // kieu ki tu 
+	
+	return 0;
+	}
