8cfde7a9e9562a66c6ac93fa3213513f69908c2f
Maihuy211 <maihuylc@gmail.com>
2024-11-20 15:26:08 +0700

    upload baitap3

---
+++ baitap3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main ()
+{
+     int a=21,b=10,tong,hieu,tich;
+	 float thuong;
+    tong = a + b;
+    hieu = a - b;
+    tich = a * b;
+    thuong = (float)a / b;
+	printf("tong= %d\n", tong);
+	printf("hieu= %d\n", hieu);
+	printf("tich= %d\n", tich);
+	printf("thuong= %.2f\n",thuong);
+	 return 0;
+	}
+  
+  
