82f837da8249f4f8f10ec4b8bf3a395f58c72aa9
Maihuy211 <maihuylc@gmail.com>
2024-11-20 15:27:32 +0700

    upload baitap6

---
+++ baitap6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+   float r=3,chu_vi,dien_tich;
+   const float pi =3.14;
+   chu_vi = pi*r*2 ;
+   dien_tich= r*r*pi ;
+   printf("chu vi hinh tron la: %.2f\n",chu_vi);
+   printf("dien tich hinh tron la: %.2f\n",dien_tich);
+   return 0;
+   }
+
+
