0ad6742c95c140d26357e25d1af87d39666cfeb4
Maihuy211 <maihuylc@gmail.com>
2024-11-20 15:26:51 +0700

    upload baitap5

---
+++ baitap5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+   int chieu_dai=4,chieu_rong=3,dien_tich,chu_vi;
+   chu_vi=(chieu_dai+chieu_rong)*2;
+   dien_tich=chieu_dai*chieu_rong;
+   printf("chu vi hinh chu nhat la: %d\n",chu_vi);
+   printf("dien tich hinh chu nhat la: %d\n",dien_tich);
+   return 0;
+   }
+
