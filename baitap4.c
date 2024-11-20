98d96ae5c385e08d70d5b54de6ef1a4699e25a23
Maihuy211 <maihuylc@gmail.com>
2024-11-20 15:26:29 +0700

    upload baitpa4

---
+++ baitap4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+   int canh=3,chu_vi,dien_tich;
+   chu_vi= canh*4;
+   dien_tich= canh*canh;
+   printf("chu vi hinh vuong: %d\n",chu_vi);
+   printf("dien tich hinh vuong: %d\n",dien_tich);
+   return 0;
+   }
+
